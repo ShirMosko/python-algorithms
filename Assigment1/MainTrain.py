28dcce81c951284b80ac06f6e64ee694f8f1f0a2
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2023-04-02 22:35:12 +0300

    add assigment files

--- Assigment1/MainTrain.py
+++ Assigment1/MainTrain.py
@@ -1,17 +1,52 @@
-from datetime import date
-from solution1 import *
+from parser import *
+import random
+import string
 
-d1 = (2024,4,2)
-d2 = (2022,11,9)
-dd1 = date(d1[0],d1[1],d1[2])
-dd2 = date(d2[0],d2[1],d2[2])
-delta = dd1 - dd2
+a = Num(random.randint(-100,100))
+b = Num(random.randint(-100,100))
+c = Num(random.randint(-100,100))
+d = Num(random.randint(-100,100))
 
-if calcDays(d1,d2) != abs(delta.days):
-    print(f"you got a wrong result for {d1} , {d2} (-50)")
+if Plus(a,b).calc() != a.calc()+b.calc():
+    print("problem with Plus (-10)")
 
-if calcDays(d2,d1) != abs(delta.days):
-    print(f"you got a wrong result for {d2} , {d1} (-50)")
+if Minus(a,b).calc() != a.calc()-b.calc():
+    print("problem with Minus (-10)")
+
+if Mul(a,b).calc() != a.calc()*b.calc():
+    print("problem with Mul (-10)")
+
+x = Div(a,b).calc()
+if x > a.calc()/b.calc()+0.01 or x < a.calc()/b.calc()-0.01:
+    print("problem with Div (-10)")
+
+if Plus(a,Mul(b,Minus(c,d))).calc() != a.calc()+b.calc()*(c.calc()-d.calc()) :
+    print("problem with expression (-10)")
+
+
+def strf(x) -> string:
+    s=str(x)
+    if s.startswith('-'):
+        s="("+s+")"
+    return s
+
+sa = strf(a.calc())
+sb = strf(b.calc())
+sc = strf(c.calc())
+sd = strf(d.calc())
+
+s = sa+'+'+sb+'*('+sc+'-'+sd+')'
+if parser(s) != eval(s) :
+    print("problem with parser (-10)")
+
+
+s = sa+'*'+sa+'+'+sb+'*('+sc+'-'+sd+'+'+sb+')'
+if parser(s) != eval(s) :
+    print("problem with parser (-20)")
+
+s = sa+'*('+sa+'+'+sb+'*('+sc+'-'+sd+'+'+sb+'))'
+if parser(s) != eval(s) :
+    print("problem with parser (-20)")
 
 
 print("done")
--- Assigment1/solution1.py
+++
@@ -1,3 +0,0 @@
-def calcDays(date1,date2):
-	sum = 0
-    return sum
